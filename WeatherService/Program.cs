293ee554a697e74a6f52f1496b4ebea5697aae44
jf-gbg <66627787+jf-gbg@users.noreply.github.com>
2025-08-26 15:10:06 +0200

    Add CORS policy for local frontend development

--- WeatherService/Program.cs
+++ WeatherService/Program.cs
@@ -4,6 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add CORS policy for local frontend dev
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LocalDevCors", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +24,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Apply CORS
+app.UseCors("LocalDevCors");
 
 var summaries = new[]
 {
